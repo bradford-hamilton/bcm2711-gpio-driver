d4763d2461e59b8c102755cdd3f6f26e25a87625
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-08-07 12:13:24 -0600

    test.c -> led-byte.c and Makefile updates for this

--- Makefile
+++ Makefile
@@ -2,17 +2,17 @@ obj-m += gpio-driver.o
 
 PWD := $(CURDIR)
 
-.PHONY: code-to-pi
+.PHONY: code-to-pi led-app
 
 code-to-pi:
-	scp -r gpio-driver.c bcm2711-gpio.dtsi gpio.h gpio.c test.c Makefile ubuntu@192.168.1.250:/home/ubuntu/bcm2711-gpio-driver
+	scp -r gpio-driver.c bcm2711-gpio.dtsi gpio.h gpio.c led-byte.c Makefile ubuntu@192.168.1.250:/home/ubuntu/bcm2711-gpio-driver
 
-# From pi session
+# From raspberry pi session
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
 
-move-dtb:
-	sudo cp bcm2711-overlay.dtbo /boot/firmware/overlays
+led-app:
+	gcc -Wall -Wextra -o main led-byte.c gpio.c
---
+++ led-byte.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "gpio.h"
+
+static char *gpio_labels[] = {"gpio6", "gpio5", "gpio25", "gpio24", "gpio23", "gpio22", "gpio27", "gpio17"};
+
+void configure_all_gpios(gpio_direction_t direction);
+void display_byte(uint8_t num);
+void clear_byte();
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <8-bit-number>\n", argv[0]);
+    return 1;
+  }
+
+  configure_all_gpios(DIRECTION_OUTPUT);
+
+  errno = 0;
+  char *end_ptr;
+  long num = strtol(argv[1], &end_ptr, 10);
+  if (errno != 0 || *end_ptr != '\0' || num < 0 || num > 255) {
+    fprintf(stderr, "Error: Invalid 8-bit number provided\n");
+    return 1;
+  }
+
+  display_byte(num);
+
+  sleep(3);
+
+  clear_byte();
+}
+
+void configure_all_gpios(gpio_direction_t direction)
+{
+  size_t gpio_labels_len = sizeof(gpio_labels) / sizeof(gpio_labels[0]);
+
+  for (int i = gpio_labels_len - 1; i >= 0; i--) {
+    gpio_configure_direction(gpio_labels[i], direction);
+  }
+}
+
+void display_byte(uint8_t num)
+{
+  size_t gpio_labels_len = sizeof(gpio_labels) / sizeof(gpio_labels[0]);
+  
+  for (int i = gpio_labels_len - 1; i >= 0; i--) {
+    size_t bit = (num >> i) & 1;
+    if (bit == 1) {
+      gpio_write_value(gpio_labels[i], VALUE_HIGH);
+    }
+  }
+}
+
+void clear_byte()
+{
+  size_t gpio_labels_len = sizeof(gpio_labels) / sizeof(gpio_labels[0]);
+
+  for (int i = gpio_labels_len - 1; i >= 0; i--) {
+    gpio_write_value(gpio_labels[i], VALUE_LOW);
+  }
+}
