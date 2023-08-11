571020c5a2b1b87422e460d1825576f713f8f329
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-08-11 08:00:58 -0600

    add some user docs to gpio.h

--- gpio.h
+++ gpio.h
@@ -13,8 +13,13 @@ typedef enum {
   VALUE_HIGH = 1,
 } gpio_value_t;
 
+// Configure the gpio pin's direction (input/output)
 int gpio_configure_direction(char *label, gpio_direction_t direction);
+
+// Write a value to a gpio pin (high/low)
 int gpio_write_value(char *label, gpio_value_t value);
+
+// Read the current value of a gpio pin
 int gpio_read_value(char *label);
 
 #endif // GPIO_H
