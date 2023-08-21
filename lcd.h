40053c4ba944445cb57acb248d23d78c7052057c
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-08-21 11:35:36 -0600

    begin lcd gpio lib

---
+++ lcd.h
@@ -0,0 +1,13 @@
+#ifndef LCD_H
+#define LCD_H
+
+// LCD pins and their corresponding GPIO
+#define LCD_GPIO_RS "gpio6"
+#define LCD_GPIO_RW "gpio5"
+#define LCD_GPIO_E  "gpio25"
+#define LCD_GPIO_D4 "gpio24"
+#define LCD_GPIO_D5 "gpio23"
+#define LCD_GPIO_D6 "gpio22"
+#define LCD_GPIO_D7 "gpio27"
+
+#endif // LCD_H
