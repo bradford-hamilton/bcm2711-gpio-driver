1d21673732858105f7fc6ee02b781a20180df652
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-09-01 11:20:48 -0600

    model out the gcd file_operations struct and methods needed

--- char/gpio-char-driver.c
+++ char/gpio-char-driver.c
@@ -1,22 +1,60 @@
 #include <linux/module.h>
+#include <linux/fs.h>
 
 #undef pr_fmt
 #define pr_fmt(fmt) "%s : " fmt,__func__
 
-static int __init gpio_char_drv_init(void)
+static int gcd_open(struct inode *inod, struct file *filp);
+static int gcd_release(struct inode *inod, struct file *filp);
+static ssize_t gcd_read(struct file *filp, char __user *user_buf, size_t count, loff_t *fpos);
+static ssize_t gcd_write(struct file *filp, const char __user *user_buf, size_t count, loff_t *fpos);
+
+static const struct file_operations gcd_fops = {
+  .open = gcd_open,
+  .release = gcd_release,
+  .read = gcd_read,
+  .write = gcd_write,
+  .owner = THIS_MODULE,
+};
+
+static int __init gcd_init(void)
 {
   pr_info("char driver initialized\n");
   return 0;
 }
 
-static void __exit gpio_char_drv_exit(void)
+static void __exit gcd_exit(void)
 {
   pr_info("char driver removed\n");
   return;
 }
 
-module_init(gpio_char_drv_init);
-module_exit(gpio_char_drv_exit);
+static int gcd_open(struct inode *inod, struct file *filp)
+{
+  pr_info("gcd opened\n");
+  return 0;
+}
+
+static int gcd_release(struct inode *inod, struct file *filp)
+{
+  pr_info("gcd released\n");
+  return 0;
+}
+
+static ssize_t gcd_read(struct file *filp, char __user *user_buf, size_t count, loff_t *fpos)
+{
+  pr_info("gcd read\n");
+  return 0;
+}
+
+static ssize_t gcd_write(struct file *filp, const char __user *user_buf, size_t count, loff_t *fpos)
+{
+  pr_info("gcd write\n");
+  return 0;
+}
+
+module_init(gcd_init);
+module_exit(gcd_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("bradford-hamilton");
